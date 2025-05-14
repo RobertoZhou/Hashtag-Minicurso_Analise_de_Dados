206bc2b62927df98adf99c663fa16a96083ef01d
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2025-05-14 01:17:52 -0300

    Preparando a Lógica de Programação do Projeto

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+    - Passo 1: Importar a base de dados
+    - Passo 2: Visualizar e tratar essa base de dados
+    - Passo 3: "Dar uma olhada" na sua base de dados
+    - passo 4: Construir uma análise oara identificar o motivo de cancelamento
+        - Identificar qual o motivo ou os principaís motivos dos clientes estarem cancelando o cartão de crédito
+"""
